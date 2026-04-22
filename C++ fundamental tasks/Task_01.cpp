1390ffc24f857c3070cc8ebdfa51123f4b7913c2
abdullahsif90 <abdullahasif8639260@gmail.com>
2026-04-22 11:40:42 +0500

    Solved Task 01: Introduction Code

---
+++ C++ fundamental tasks/Task_01.cpp
@@ -0,0 +1,12 @@
+// This code tells you about myself 
+// course c++ fundamentals
+
+#include <iostream>
+using namespace std;
+int main (){
+    cout << "My name is Abdullah Asif "<<endl;
+    cout << "I am 20 years old"<<endl;
+    cout<<"I did my fsc in 2023 from Kips College Multan "<<endl;
+    cout<<"I am currently pursuing my bachelors in Computer Engineering from International Islamic University Islamabad "<<endl;
+    return 0;
+}
