473fd6586b28e0ab118358b98765d9da645d9436
abdullahsif90 <abdullahasif8639260@gmail.com>
2026-04-22 14:56:59 +0500

    Added Lab 2 Boat Pattern Task

---
+++ C++ fundamental tasks/Task_2.cpp
@@ -0,0 +1,19 @@
+// This is Task 01: Boat Pattern 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "          --          " << endl;
+    cout << "         /||          " << endl;
+    cout << "        / ||          " << endl;
+    cout << "       /  ||          " << endl;
+    cout << "      /   ||          " << endl;
+    cout << "     -----||          " << endl;
+    cout << "          ||          " << endl;
+    cout << "   _______||_______   " << endl;
+    cout << "   \\              /   " << endl;
+    cout << "    \\            /    " << endl;
+    cout << "     \\__________/     " << endl;
+
+    return 0;
+}
